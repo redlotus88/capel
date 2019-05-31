17a36775f5536395914efc2cd03354fb332a9522
DragonWang <185138868@qq.com>
2019-05-31 23:01:12 +0800

    提交BlockingDeque的使用案例

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by dragon on 2019/5/22.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\u87ba\u4e1d\u6279");
+        System.out.println("\u7535\u52a8\u5de5\u5177");
+
+    }
+}
