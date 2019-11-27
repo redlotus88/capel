357d878c30845e22101e2ec948a13e87bf23ef81
DragonWang <185138868@qq.com>
2019-11-27 14:23:28 +0800

    更新Synchronized解答

--- src/main/java/com/c8n/aliyun/terminnology/Synchonized.java
+++ src/main/java/com/c8n/aliyun/terminnology/Synchonized.java
@@ -17,6 +17,9 @@ public class Synchonized {
          * 方法上的Synchronized是JVM通过标识ACC_SYNCHRONIZED来识别同步方法的.
          *
          * 参考资料: https://baijiahao.baidu.com/s?id=1612142459503895416&wfr=spider&for=pc
+         *
+         * 阿里答案: 解决并发情况下数据同步访问的一个很重要的关键字。保证一个共享资源在同一时间只会被一个线程访问到时，
+         *          可以在代码中使用synchronized关键字对类或者对象加锁
          */
 
         synchronized (Synchonized.resource) {
