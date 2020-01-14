66364024e5a16d7eccc703dbe9147bbd1bf21be1
DragonWang <185138868@qq.com>
2020-01-14 17:11:49 +0800

    分布式锁测试提交

--- src/test/java/com/c8n/aliyun/terminnology/DistributedLockTests.java
+++ src/test/java/com/c8n/aliyun/terminnology/DistributedLockTests.java
@@ -4,6 +4,7 @@ import com.c8n.ApplicationTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,9 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 @Slf4j
 public class DistributedLockTests {
 
-    @Test
-    public void test() {
-        log.info("test");
+    @Autowired
+    private DistributedLock distributedLock;
 
+    @Test
+    public void test() throws InterruptedException {
+        distributedLock.runExampleBasedOnRedis();
     }
 }
