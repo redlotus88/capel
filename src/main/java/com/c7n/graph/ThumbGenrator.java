f03e23a4b5fd775f09a738d57bd3b46024b21f35
DragonWang <185138868@qq.com>
2020-07-02 17:48:13 +0800

    缩略图生成

---
+++ src/main/java/com/c7n/graph/ThumbGenrator.java
@@ -0,0 +1,18 @@
+package com.c7n.graph;
+
+import net.coobird.thumbnailator.Thumbnails;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 缩略图生成
+ */
+public class ThumbGenrator {
+
+    public static void main(String[] args) throws IOException {
+        File picPath = new File("/Users/dragon/Desktop/衣服尺码.jpeg");
+        Thumbnails.of(picPath).size(600,480).outputFormat("jpg").toFile("tmp/thumbnails_");
+
+    }
+}
