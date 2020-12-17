c2ad19dc4a264a9ccb427a1eed26ebadb1a4a9e4
DragonWang <185138868@qq.com>
2020-12-17 09:56:00 +0800

    jmh测试性能工具使用

---
+++ src/main/java/com/c7n/jmh/StringConnectTest.java
@@ -0,0 +1,60 @@
+package com.c7n.jmh;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 字符串连接测试
+ * 用来判断 + 和 StringBuilder.append() 两种字符串拼接哪个耗时更短
+ *
+ * 参考文章：https://www.zhihu.com/question/276455629/answer/1259967560
+ *
+ * @author jialong.wang
+ * @Date on 2020/12/17 9:37 AM
+ * @since 1.0
+ */
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 3, time = 1)
+@Measurement(iterations = 5, time = 5)
+@Threads(4)
+@Fork(1)
+@State(value = Scope.Benchmark)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class StringConnectTest {
+
+    @Param(value = {"10", "50", "100"})
+    private int length;
+
+    @Benchmark
+    public void testStringAdd(Blackhole blackhole) {
+        String a = "";
+        for (int i = 0; i < length; i++) {
+            a += i;
+        }
+        blackhole.consume(a);
+    }
+
+    @Benchmark
+    public void testStringBuilderAdd(Blackhole blackhole) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(i);
+        }
+        blackhole.consume(sb.toString());
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(StringConnectTest.class.getSimpleName())
+                .result("result.json")
+                .resultFormat(ResultFormatType.JSON).build();
+        new Runner(opt).run();
+    }
+}
